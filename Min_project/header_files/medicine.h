f79dcbf968cdf13831fb720f7dc91ea4e52d740b
mukthabejjaram12-lang <mukthabejjaram12@gmail.com>
2026-08-18 16:22:07 +0530

    Add files via upload

---
+++ Min_project/header_files/medicine.h
@@ -0,0 +1,7 @@
+#include"types.h"
+s32 read_two_digit(void);
+s32 read_four_digit(void);
+s32 read_single_digit(void);
+void configure_medicine(void);
+void alert_user(void);
+void check_medicine_time(s32,s32);
---
+++ Min_project/header_files/rtc_edit.h
@@ -0,0 +1,7 @@
+//rtc_edit.h
+#ifndef RTC_EDIT_H
+#define RTC_EDIT_H
+
+void edit_rtc_time(void);
+
+#endif
